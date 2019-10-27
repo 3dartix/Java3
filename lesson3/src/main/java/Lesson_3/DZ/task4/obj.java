c94f39868141d8e0f81645cf2b05ae91ad5e6b13
3dartix <info@3dartix.ru>
2019-10-27 13:12:08 +0200

    cont task4

---
+++ lesson3/src/main/java/Lesson_3/DZ/task4/obj.java
@@ -0,0 +1,13 @@
+package Lesson_3.DZ.task4;
+
+import java.io.Serializable;
+
+public class obj implements Serializable {
+    public int id;
+    public String name;
+
+    public obj(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ lesson3/src/main/java/Lesson_3/DZ/task4/server.java
@@ -0,0 +1,73 @@
+package Lesson_3.DZ.task4;
+
+import com.sun.security.ntlm.Client;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+    public static void main(String[] args) {
+        new MainServer();
+    }
+}
+
+class MainServer{
+    ServerSocket server = null;
+    Socket socket = null;
+
+    private DataInputStream in; //входящий поток
+    private DataOutputStream out; //исходящий поток
+
+    public MainServer(){
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен!");
+
+
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился!");
+                in = new DataInputStream(socket.getInputStream());
+                out = new DataOutputStream(socket.getOutputStream());
+                break;
+            }
+
+
+            FileOutputStream fos = new FileOutputStream("dz/task4/receivedFile.my");
+
+            while (in.read() != -1) {
+                System.out.println(in.read());
+            }
+
+
+            System.out.println("все");
+            fos.close();
+
+            ObjectInputStream ios = new ObjectInputStream( new FileInputStream("dz/task4/receivedFile.my"));
+            try {
+                obj ob = (obj) ios.readObject();
+                System.out.println(ob.id + "\n" + ob.name);
+                ios.close();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Файл получен, закрываем");
+            out.writeUTF("/ok");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+                out.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
